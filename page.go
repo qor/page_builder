b12338769cad93be25b186c672469cabadc0a264
Jinzhu <wosmvp@gmail.com>
2017-06-07 12:04:22 +0800

    Add page model

---
+++ page.go
@@ -0,0 +1,25 @@
+package page_builder
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/qor/admin"
+	"github.com/qor/qor/resource"
+	"github.com/qor/slug"
+	"github.com/qor/sorting"
+	"github.com/qor/widget"
+)
+
+type Page struct {
+	gorm.Model
+	Title         string
+	TitleWithSlug slug.Slug
+
+	QorWidgetSettings       []widget.QorWidgetSetting `gorm:"many2many:page_qor_widget_settings;ForeignKey:id;AssociationForeignKey:name"`
+	QorWidgetSettingsSorter sorting.SortableCollection
+}
+
+func (*Page) ConfigureQorResource(res resource.Resourcer) {
+	if res, ok := res.(*admin.Resource); ok {
+		res.UseTheme("page_builder")
+	}
+}
